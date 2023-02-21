32f5baf21fb1df553d732190c44a6e64c595fa26
Hussien12Mostafa <85521642+Hussien12Mostafa@users.noreply.github.com>
2023-02-21 22:17:45 +0200

    first version from my protofolio

---
+++ lib/changes/links.dart
@@ -0,0 +1,29 @@
+// CV
+const String resume =
+    'https://drive.google.com/drive/folders/1tkATM4xAaFUDw7hQvMdAxephYB48951H?usp=share_link';
+
+// Github Link
+const String gitHub = 'https://github.com/Hussien12Mostafa';
+// Github Link
+const String facebook = 'https://www.facebook.com/profile.php?id=100009163527705';
+// Github Link
+//const String twitter = 'https://twitter.com/sudesh78';
+// Github Link
+// const String youtube =
+//     'https://www.youtube.com/channel/UCXooUY2oL_eqGhTaZn-ExSg';
+// Github Link
+const String linkedin = 'https://www.linkedin.com/in/hussien-awad-b6343a21a/';
+// Github Link
+//const String buymeacoffee = 'https://www.buymeacoffee.com/sudeshnb';
+// Github Link
+//const String instagram = 'https://instagram.com/sudesh_nish';
+
+// const String profileImage =
+//     "https://images.unsplash.com/photo-1503443207922-dff7d543fd0e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80";
+//
+// Github Link
+// const String fiverr = 'https://www.fiverr.com/sudeshnb';
+// Github Link
+const String upwork = 'https://www.upwork.com/freelancers/~01b7ff796173b649cd';
+// Github Link
+const String whatsapp = 'https://api.whatsapp.com/send?phone=2001157421059';
